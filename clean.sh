79d89b002195c82d0c6fb9b21f85cf0d9384370b
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-09-26 20:12:27 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,12 @@
+cd $(dirname $0)
+
+if [[ "$PWD" != "$HOME/.config/nvim" ]];
+then
+	echo "Error: Your Neovim config is not located at '~/.config/nvim'"
+	exit 1
+fi
+echo "Your Neovim config is located at '~/.config/nvim'"
+
 cd ~
 sudo rm -rd ~/.local/share/nvim
 sudo snap remove nvim
