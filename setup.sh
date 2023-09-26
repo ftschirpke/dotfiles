dbfc0b12a3f0f7dc9b9a2e95a9a39e4856337cce
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-09-26 19:49:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,15 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 
 echo "--------------------------------------------------------------------------------------"
 echo "Installing nodejs (required for Copilot)"
-sudo snap install node --classic
+sudo apt-get remoe nodejs -y
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 echo "======================================================================================"
 echo "REMEMBER:"
