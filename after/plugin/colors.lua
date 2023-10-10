3ab9e0a6346806cfb4ea8067a525a493e734a5e2
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-10-10 15:45:53 +0200

    feat: brighter line numbers

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,6 +5,7 @@ require('ayu').setup({
         -- light grey: #292D33, #1B2733
         ColorColumn = { bg = '#1B2733' },
         CursorLine = { bg = '#402410' },
+        LineNr = { fg = '#5C6773' },
     },
 })
 vim.cmd [[colorscheme ayu-dark]]
