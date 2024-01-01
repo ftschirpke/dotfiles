fa4415bc9f68f02d6203983094d112624a701006
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2024-01-01 19:54:25 +0100

    feat: function and shortcut for swapping to and from header in c++

---
+++ lua/friedrich/cpp_header_impl_switch.lua
@@ -0,0 +1,11 @@
+local switch_impl_header = function()
+    local fileroot = vim.fn.expand('%:r')
+    local extension = vim.fn.expand('%:e')
+    if (extension == 'cpp') then
+        vim.cmd(":e " .. fileroot .. ".hpp")
+    elseif (extension == 'hpp') then
+        vim.cmd(":e " .. fileroot .. ".cpp")
+    end
+end
+
+vim.keymap.set("n", "<leader>h", switch_impl_header)
