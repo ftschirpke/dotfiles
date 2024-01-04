82b7cd1eff1e00c6412f23e5327141142db471f9
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2024-01-04 11:47:47 +0100

    fix: do not replace ' in remaps

--- lua/friedrich/remap.lua
+++ lua/friedrich/remap.lua
@@ -2,8 +2,8 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- move lines
-vim.keymap.set("v", "J", ":m ">+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m "<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- center after moving
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
