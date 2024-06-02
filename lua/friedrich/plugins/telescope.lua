e8da0fe2228dab43ca2c5fd1fc74bf825cbf31f1
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2024-06-02 12:22:54 +0200

    fix: deprecated trouble telescope interaction

--- lua/friedrich/plugins/telescope.lua
+++ lua/friedrich/plugins/telescope.lua
@@ -4,12 +4,12 @@ return {
         tag = "0.1.7",
         dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
         config = function()
-            local trouble = require("trouble.providers.telescope")
+            local trouble = require("trouble.sources.telescope")
             require("telescope").setup({
                 defaults = {
                     mappings = {
-                        i = { ["<c-t>"] = trouble.open_with_trouble },
-                        n = { ["<c-t>"] = trouble.open_with_trouble },
+                        i = { ["<c-t>"] = trouble.open },
+                        n = { ["<c-t>"] = trouble.open },
                     },
                 },
             })
