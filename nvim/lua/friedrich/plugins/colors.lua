9daf6225875f5354e05405a7f1234e543e17f60a
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2024-10-17 16:57:13 +0200

    fix: personalized colors

--- nvim/lua/friedrich/plugins/colors.lua
+++ nvim/lua/friedrich/plugins/colors.lua
@@ -1,18 +1,9 @@
-local ayu_options = {
-    mirage = false,
-    overrides = {
-        -- use ":source $VIMRUNTIME/syntax/hitest.vim" to see the highlight groups
-        -- light grey: #292D33, #1B2733
-        ColorColumn = { bg = "#1B2733" },
-        CursorLine = { bg = "#402410" },
-        LineNr = { fg = "#5C6773" },
-    },
-}
+-- use ":source $VIMRUNTIME/syntax/hitest.vim" to see the highlight groups
 
 return {
-    "Shatur/neovim-ayu",
+    "ftschirpke/neovim-ayu",
     config = function()
-        require("ayu").setup(ayu_options)
+        require("ayu").setup({ mirage = false })
         vim.cmd [[colorscheme ayu-dark]]
         vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#1B2733" })
     end,
