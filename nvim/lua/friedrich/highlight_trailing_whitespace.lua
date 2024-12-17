7ef170f219f4c32ffa25b73ac8eff3deb1430908
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2024-12-17 15:01:10 +0100

    fix: remove trailing whitespace highlight

--- nvim/lua/friedrich/highlight_trailing_whitespace.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_set_hl(0, 'TrailingWhitespace', { fg = 'red', bg = 'red' })
-vim.api.nvim_create_autocmd('BufEnter', {
-    pattern = '*',
-    callback = function()
-        vim.fn.matchadd('TrailingWhitespace', '\\s\\+$')
-    end,
-})
