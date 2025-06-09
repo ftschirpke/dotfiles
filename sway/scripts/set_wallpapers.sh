eac28759099ce16883f9fbcc03ac9972481eacfd
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-06-10 00:19:41 +0200

    fix: persistent wallpaper choice

--- sway/scripts/set_wallpapers.sh
+++ sway/scripts/set_wallpapers.sh
@@ -4,15 +4,10 @@ WALLPAPER_DIR="$HOME/wallpapers"
 INDEX_FILE="$HOME/.config/sway/.wallpaper_index"
 WALLPAPERS=($WALLPAPER_DIR/wp*.jpg)
 
-TOTAL=${#WALLPAPERS[@]}
-
 if [ ! -f "$INDEX_FILE" ]; then
-    echo 0 > "$INDEX_FILE"
+    exit 0
 fi
 
 INDEX=$(cat "$INDEX_FILE")
-INDEX=$(( (INDEX + 1) % TOTAL ))
-echo $INDEX > "$INDEX_FILE"
 
-set_wallpapers.sh
-
+swaymsg output "*" bg "${WALLPAPERS[$INDEX]}" fill
