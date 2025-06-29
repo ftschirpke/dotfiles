b2e8fbcc39c4c96722e4acfae5ca66f1126528d5
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-06-29 21:49:59 +0200

    fix: rust-analyzer bug

--- nvim/lua/friedrich/plugins/lsp.lua
+++ nvim/lua/friedrich/plugins/lsp.lua
@@ -99,6 +99,16 @@ local rust_analyzer_handler = function()
     })
 end
 
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
+
 local configure_mason = function()
     require("mason").setup({})
     require("mason-lspconfig").setup({
