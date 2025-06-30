116707c68d34c3f9ab339c7eee3f3deaa43b6ed6
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-06-30 16:24:28 +0200

    fix: single border for hover windows in new neovim version

--- nvim/lua/friedrich/plugins/lsp.lua
+++ nvim/lua/friedrich/plugins/lsp.lua
@@ -29,6 +29,8 @@ local configure_lsp_zero = function()
         })
     })
 
+    vim.o.winborder = "single"
+
     lsp_zero.on_attach(function(_, bufnr)
         lsp_zero.default_keymaps({ buffer = bufnr })
         local opts = { buffer = bufnr, remap = false }
