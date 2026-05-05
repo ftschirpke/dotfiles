60bf33d00cdc1f3f9a38851440032872ef09cc5b
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2026-05-05 23:35:31 +0200

    fix: telescope and whitespace

--- nvim/lua/friedrich/plugins/telescope.lua
+++ nvim/lua/friedrich/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
+        tag = "v0.2.1",
         dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
         config = function()
             local trouble = require("trouble.sources.telescope")
